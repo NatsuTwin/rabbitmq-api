3d6fd5119ea4ed42bd9a10082a73a133a7111048
NatsuTwin <icemerpro@gmail.com>
2022-07-27 11:07:57 +0200

    Made CompletableFuture complete when the request timeouts. 🎀

--- src/main/java/fr/playfull/rmq/protocol/client/RPCClient.java
+++ src/main/java/fr/playfull/rmq/protocol/client/RPCClient.java
@@ -1,32 +1,33 @@
 package fr.playfull.rmq.protocol.client;
 
 import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
 import fr.playfull.rmq.RabbitMQAPI;
 import fr.playfull.rmq.query.RPCRequest;
 import fr.playfull.rmq.query.Request;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.ForkJoinPool;
 
 public class RPCClient extends Client {
 
     @Override
     public void send(Request request) {
         getThreadPool().execute(() -> {
+            // Name the thead.
             Thread.currentThread().setName("rpc-client-thread" + UUID.randomUUID());
+            // If the request isn't an RPC request
+            if(!(request instanceof RPCRequest rpcRequest))
+                return;
             try {
-
-                // If the request is not a RPC request
-                if(!(request instanceof RPCRequest rpcRequest))
-                    return;
-
+                // Message publication. (Managed in single thread).
                 String correlationId = UUID.randomUUID().toString();
-
                 String replyQueueName = getChannel().queueDeclare().getQueue();
+
                 AMQP.BasicProperties properties = new AMQP.BasicProperties
                         .Builder()
                         .correlationId(correlationId)
@@ -36,27 +37,48 @@ public class RPCClient extends Client {
 
                 // We publish our request data
                 getChannel().basicPublish("", request.getQueue(), properties, RabbitMQAPI.getBufferManager().serialize(request.getPayload()));
-                RabbitMQAPI.getLogger().info("[Client] Sending request in queue " + request.getQueue());
-
-                BlockingQueue<Object> response = new ArrayBlockingQueue<>(1);
-
-                String consumerTag = getChannel().basicConsume(replyQueueName, true, (cTag, delivery) -> {
-                    if (delivery.getProperties().getCorrelationId().equals(correlationId)) {
-                        // We take the offer
-                        response.offer(RabbitMQAPI.getBufferManager().deserialize(delivery.getBody()));
+                RabbitMQAPI.getLogger().info("[Client] Request sent in queue " + request.getQueue());
+                // Answer handling.
+                ForkJoinPool.commonPool().execute(() -> {
+                    try {
+                        BlockingQueue<Object> response = new ArrayBlockingQueue<>(1);
+                        String consumerTag = getChannel().basicConsume(replyQueueName, true, (cTag, delivery) -> {
+                            if (delivery.getProperties().getCorrelationId().equals(correlationId)) {
+                                // We take the offer
+                                response.add(RabbitMQAPI.getBufferManager().deserialize(delivery.getBody()));
+                            }
+                        }, cTag -> {});
+                        // Mark now instant.
+                        Instant start = Instant.now();
+                        // We take the result
+                        Object result = response.poll(rpcRequest.getRequestTimeout().getTimeout(), rpcRequest.getRequestTimeout().getTimeUnit());
+                        // Calculates the answer speed in millis.
+                        long duration = Duration.between(start, Instant.now()).toMillis();
+                        // Destroy channel
+                        getChannel().basicCancel(consumerTag);
+                        // Check if we timed out.
+                        if(result == null) {
+                            // Notice the console that we timed out.
+                            RabbitMQAPI.getLogger().info("[Client] " + request.getQueue() + " timed out in " + duration + "ms.");
+                            // Notice the client that we timed out.
+                            rpcRequest.getRequestTimeout().getConsumer().accept(true);
+                            rpcRequest.getRequestAnswer().getFuture().complete(null);
+                        } else {
+                            RabbitMQAPI.getLogger().info("[Client] Received answer in queue " + request.getQueue() + " in " + duration + "ms.");
+                            // We consume the answer
+                            rpcRequest.getRequestAnswer().getConsumer().accept(result);
+                            rpcRequest.getRequestAnswer().getFuture().complete(result);
+                        }
+                    } catch (IOException | InterruptedException exception) {
+                        RabbitMQAPI.getLogger().error(exception.getMessage());
+                        // Notice the client that something wrong happened.
+                        rpcRequest.getRequestTimeout().getConsumer().accept(true);
                     }
-                }, cTag -> {
                 });
-
-                // We take the result
-                Object result = response.poll(rpcRequest.getRequestTimeout().getTimeout(), rpcRequest.getRequestTimeout().getTimeUnit());
-                getChannel().basicCancel(consumerTag);
-
-                RabbitMQAPI.getLogger().info("[Client] Received answer in queue " + request.getQueue());
-                // We consume the answer
-                rpcRequest.getRequestAnswer().getConsumer().accept(result);
-            } catch (IOException | InterruptedException exception) {
-                RabbitMQAPI.getLogger().severe(exception.getMessage());
+            } catch (IOException exception) {
+                RabbitMQAPI.getLogger().error(exception.getMessage());
+                // Notice the client that something wrong happened.
+                rpcRequest.getRequestTimeout().getConsumer().accept(true);
             }
         });
     }
