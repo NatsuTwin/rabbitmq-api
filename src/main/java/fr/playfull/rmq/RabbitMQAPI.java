5fe935ac8c036293e01e3ea208eef7c77c0d2e63
NatsuTwin <icemerpro@gmail.com>
2022-07-28 19:29:41 +0200

    Modified the main Facade and its way to handle the registration class. 🔦

--- src/main/java/fr/playfull/rmq/RabbitMQAPI.java
+++ src/main/java/fr/playfull/rmq/RabbitMQAPI.java
@@ -1,82 +1,53 @@
 package fr.playfull.rmq;
 
-import fr.playfull.rmq.connect.Connector;
-import fr.playfull.rmq.event.EventBus;
 import fr.playfull.rmq.event.protocol.ProtocolEvent;
 import fr.playfull.rmq.event.protocol.ProtocolListener;
-import fr.playfull.rmq.forward.Forwarder;
 import fr.playfull.rmq.protocol.ProtocolType;
 import fr.playfull.rmq.protocol.Side;
 import fr.playfull.rmq.query.Request;
-import fr.playfull.rmq.serializer.ByteSerializableBufferManager;
 import fr.playfull.rmq.serializer.RMQSerializable;
 import fr.playfull.rmq.serializer.factory.SerializableFactory;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.concurrent.ExecutorService;
 
 public class RabbitMQAPI {
 
-    private static RabbitMQMediator mediator;
-    private static RabbitMQAPI instance;
-    private RabbitMQAPI(RabbitMQMediator aMediator) {
-        instance = this;
-        mediator = aMediator;
+    private static RabbitMQRegistration registration;
+    private RabbitMQAPI(RabbitMQRegistration aMediator) {
+        registration = aMediator;
     }
 
-    @Nullable
-    private static RabbitMQAPI getInstance() {
-        return instance != null ? instance : null;
+    public static void disconnectAll() {
+        registration.getMediator().disconnectAll();
+    }
+
+    public static void connectAll() {
+        // Then we connect.
+        registration.getMediator().connectAll();
     }
 
     public static RabbitMQAPI hook(String filePath) throws IOException {
-        return new RabbitMQAPI(new RabbitMQMediator(filePath));
+        return new RabbitMQAPI(new RabbitMQRegistration(filePath));
     }
 
-    public static RabbitMQAPI setThreadPool(ProtocolType type, Side side, ExecutorService threadPool) {
-        getConnector().overrideThreadPool(type, side, threadPool);
-        return getInstance();
+    public static void setThreadPool(ProtocolType type, Side side, ExecutorService threadPool) {
+        registration.getMediator().allocate(type, side, threadPool);
     }
 
-    public static RabbitMQAPI sendRequest(Request request) {
-        getForwarder().send(request);
-        return getInstance();
+    public static void sendRequest(Request request) {
+        registration.getMediator().send(request);
     }
 
-    public static RabbitMQAPI listen(ProtocolType protocolType, String queueName) {
-        getForwarder().listen(protocolType, queueName);
-        return getInstance();
+    public static void listen(ProtocolType protocolType, String queueName) {
+        registration.getMediator().listen(protocolType, queueName);
     }
 
-    public static <T extends ProtocolEvent> RabbitMQAPI subscribe(Class<T> eventClass, ProtocolListener<T> event) {
-        getEventBus().subscribe(eventClass, event);
-        return getInstance();
+    public static <T extends ProtocolEvent> void subscribe(Class<T> eventClass, ProtocolListener<T> event) {
+        registration.getEventBus().subscribe(eventClass, event);
     }
 
-    public static <T extends RMQSerializable> RabbitMQAPI addFactory(Class<T> clazz, SerializableFactory<T> factory) {
-        getBufferManager().addFactory(clazz, factory);
-        return getInstance();
-    }
-
-    @Deprecated
-    public static ByteSerializableBufferManager getBufferManager() {
-        return mediator.getBufferManager();
-    }
-
-    @Deprecated
-    public static Forwarder getForwarder() {
-        return mediator.getForwarder();
-    }
-
-    @Deprecated
-    public static EventBus getEventBus() {
-        return mediator.getEventBus();
-    }
-
-    @Deprecated
-    public static Connector getConnector() {
-        return mediator.getConnector();
+    public static <T extends RMQSerializable> void addFactory(Class<T> clazz, SerializableFactory<T> factory) {
+        registration.getBufferManager().addFactory(clazz, factory);
     }
 }
--- src/main/java/fr/playfull/rmq/RabbitMQRegistration.java
+++ src/main/java/fr/playfull/rmq/RabbitMQRegistration.java
@@ -7,7 +7,8 @@ import fr.playfull.rmq.event.EventBus;
 import fr.playfull.rmq.forward.Forwarder;
 import fr.playfull.rmq.io.DefaultFileEditor;
 import fr.playfull.rmq.io.FileEditor;
-import fr.playfull.rmq.protocol.ProtocolBucket;
+import fr.playfull.rmq.protocol.ProtocolMediator;
+import fr.playfull.rmq.protocol.store.ProtocolBucket;
 import fr.playfull.rmq.serializer.ByteSerializableBufferManager;
 import fr.playfull.rmq.serializer.DefaultByteSerializableBufferManager;
 import org.slf4j.Logger;
@@ -17,36 +18,25 @@ import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 
-public class RabbitMQMediator {
+public class RabbitMQRegistration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("rmq-logger");
 
-    private final Connector connector;
-    private final Forwarder forwarder;
+    private final ProtocolMediator protocolMediator;
     private final EventBus eventBus = new EventBus();
     private final ByteSerializableBufferManager bufferManager = new DefaultByteSerializableBufferManager();
 
-    protected RabbitMQMediator(String path) throws IOException {
+    protected RabbitMQRegistration(String path) throws IOException {
         // We manage the forwarder loading.
-        ProtocolBucket protocolBucket = new ProtocolBucket();
-        connector = new DefaultConnector(protocolBucket);
-        forwarder = new Forwarder(protocolBucket);
-
-        // We manage the file creation.
+        ProtocolBucket protocolBucket = new ProtocolBucket(this);
+        // We also read the data.
         FileEditor fileEditor = new DefaultFileEditor();
         File createdFile = fileEditor.create(path, "credentials.yml");
-        Credentials credentials = fileEditor.read(createdFile);
 
-
-        // We manage the connection.
-        connector.connectAll(credentials);
+        this.protocolMediator = ProtocolMediator.of(fileEditor.read(createdFile), protocolBucket);
 
         // We set the shutdown hook
-        Runtime.getRuntime().addShutdownHook(new Thread(connector::disconnectAll));
-    }
-
-    public static RabbitMQMediator hook(String path) throws IOException {
-        return new RabbitMQMediator(path);
+        Runtime.getRuntime().addShutdownHook(new Thread(protocolMediator::disconnectAll));
     }
 
     public static Logger getLogger() {
@@ -57,16 +47,11 @@ public class RabbitMQMediator {
         return bufferManager;
     }
 
-    public Forwarder getForwarder() {
-        return forwarder;
+    public ProtocolMediator getMediator() {
+        return this.protocolMediator;
     }
 
     public EventBus getEventBus() {
         return eventBus;
-    }
-
-    @Nullable
-    public Connector getConnector() {
-        return connector;
     }
 }
