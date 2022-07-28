ad4fa82ad3c51409e24a909a5658e5bbbece5d2e
NatsuTwin <icemerpro@gmail.com>
2022-07-28 17:29:01 +0200

    Added chained statements. 💽

--- src/main/java/fr/playfull/rmq/RabbitMQAPI.java
+++ src/main/java/fr/playfull/rmq/RabbitMQAPI.java
@@ -10,34 +10,46 @@ import fr.playfull.rmq.protocol.Side;
 import fr.playfull.rmq.query.Request;
 import fr.playfull.rmq.serializer.ByteSerializableBufferManager;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 
 public class RabbitMQAPI {
 
     private static RabbitMQMediator mediator;
+    private static RabbitMQAPI instance;
     private RabbitMQAPI(RabbitMQMediator aMediator) {
+        instance = this;
         mediator = aMediator;
+    }
+
+    @Nullable
+    private static RabbitMQAPI getInstance() {
+        return instance != null ? instance : null;
     }
 
     public static RabbitMQAPI hook(String filePath) throws IOException {
         return new RabbitMQAPI(new RabbitMQMediator(filePath));
     }
 
-    public static void setThreadPool(ProtocolType type, Side side, ExecutorService threadPool) {
+    public static RabbitMQAPI setThreadPool(ProtocolType type, Side side, ExecutorService threadPool) {
         getConnector().overrideThreadPool(type, side, threadPool);
+        return getInstance();
     }
 
-    public static void sendRequest(Request request) {
+    public static RabbitMQAPI sendRequest(Request request) {
         getForwarder().send(request);
+        return getInstance();
     }
 
-    public static void listen(ProtocolType protocolType, String queueName) {
+    public static RabbitMQAPI listen(ProtocolType protocolType, String queueName) {
         getForwarder().listen(protocolType, queueName);
+        return getInstance();
     }
 
-    public static <T extends ProtocolEvent> void subscribe(Class<T> eventClass, ProtocolListener<T> event) {
+    public static <T extends ProtocolEvent> RabbitMQAPI subscribe(Class<T> eventClass, ProtocolListener<T> event) {
         getEventBus().subscribe(eventClass, event);
+        return getInstance();
     }
 
     @Deprecated
